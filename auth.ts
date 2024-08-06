6ce15fae1a464cb62c1d1cfde250367054e71da9
gonzalosalmeron <gonzxlosalmeron@gmail.com>
2024-08-06 02:33:52 +0200

    feat: Add new user domain model

--- auth.ts
+++ auth.ts
@@ -1,11 +1,22 @@
 import authConfig from './auth.config'
 import { PrismaAdapter } from '@auth/prisma-adapter'
-import NextAuth from 'next-auth'
+import { Role } from '@prisma/client'
+import NextAuth, { type DefaultSession } from 'next-auth'
 import Credentials from 'next-auth/providers/credentials'
 
 import { getUserFromDb } from '@/libs/actions'
 import prisma from '@/libs/db'
 import { isPasswordValid } from '@/libs/hash'
+
+export type ExtendedUser = DefaultSession['user'] & {
+  role: Role
+}
+
+declare module 'next-auth' {
+  interface Session {
+    user: ExtendedUser
+  }
+}
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   trustHost: true,
