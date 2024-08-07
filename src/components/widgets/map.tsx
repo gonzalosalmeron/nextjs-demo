6d2cd3a5036d1c7a6e32663dda4761aceba1d777
gonzalosalmeron <gonzxlosalmeron@gmail.com>
2024-08-07 03:09:59 +0200

    fix: Map center not moving when coordinates changes

--- src/components/widgets/map.tsx
+++ src/components/widgets/map.tsx
@@ -7,27 +7,38 @@ import { LatLngExpression } from 'leaflet'
 import 'leaflet-defaulticon-compatibility'
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css'
 import 'leaflet/dist/leaflet.css'
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
 
-export default function Map() {
-  const position = [36.72, -4.42]
+export default function Map({
+  coordinates = [36.72, -4.42],
+}: {
+  coordinates: [number, number]
+}) {
+  const position = coordinates as LatLngExpression
 
   return (
     <Card icon={<MapPinIcon className='h-4 w-4' />} title='Location'>
       <MapContainer
-        center={position as LatLngExpression}
+        center={position}
         zoom={12}
         scrollWheelZoom={true}
-        style={{ height: '400px', width: '350px' }}
+        style={{ height: '286px', width: '100%' }}
       >
+        <MapEvents coordinates={coordinates} />
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
         />
-        <Marker position={position as LatLngExpression}>
+        <Marker position={position}>
           <Popup>Weather location</Popup>
         </Marker>
       </MapContainer>
     </Card>
   )
+}
+
+const MapEvents = ({ coordinates }: { coordinates: [number, number] }) => {
+  const map = useMap()
+  map.setView(coordinates)
+  return null
 }
