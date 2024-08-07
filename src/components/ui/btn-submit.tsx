78d315501ac427a496cac69f525d3322ee8caff3
gonzalosalmeron <gonzxlosalmeron@gmail.com>
2024-08-07 18:46:58 +0200

    fix(btn-submit): Not allow submit when loading

--- src/components/ui/btn-submit.tsx
+++ src/components/ui/btn-submit.tsx
@@ -6,7 +6,10 @@ export default function BtnSubmit({ children }: { children: React.ReactNode }) {
   const { pending } = useFormStatus()
 
   return (
-    <button className='mt-2 w-full rounded-lg bg-primary px-4 py-2 text-secondary'>
+    <button
+      className='mt-2 w-full rounded-lg bg-primary px-4 py-2 text-secondary disabled:cursor-not-allowed'
+      disabled={pending}
+    >
       {pending ? 'Espere...' : children}
     </button>
   )
